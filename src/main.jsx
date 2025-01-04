af7c3a12de16b5c3f70deaa71f27c575a84fa1db
vatr84 <vatr@hotmail.fr>
2025-01-04 19:29:17 +0100

    pages/home

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import { Router as Routes } from './routes/router'
-//import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,4 @@
-import App from '../pages/App'
+import App from '../pages/home/Home'
 
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router'
 
