6232353e5cdbad5de9bb3e4ab4aad536cb004d09
vatr84 <vatr@hotmail.fr>
2025-01-15 09:39:15 +0100

    Accomodation

---
+++ src/pages/accommodation/Accommodation.jsx
@@ -0,0 +1,94 @@
+import Accordion from '../../components/accordion/Accordion'
+import Loader from '../../components/loader/Loader'
+import Person from '../../components/person/Person'
+import Rating from '../../components/Rating'
+import TagList from '../../components/taglist/TagList'
+import logementsData from '../../data/logements.json'
+import { useScrollToTop } from '../../hooks/useScrollToTop'
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import Carousel from '../../components/carousel/Carousel'
+import { useFetchData } from '../../hooks/useFetchData'
+import './Accommodation.scss'
+
+/**
+ * Custom hook to fetch accommodation data based on the provided URL.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Object} The loading state and the accommodation data.
+ */
+const useAccommodation = (url) => {
+    const { isLoading, data: accommodations } = useFetchData(url)
+    const { id } = useParams()
+    const accommodation = Array.isArray(accommodations)
+        ? accommodations.find((acc) => acc.id === id)
+        : null
+
+    return { isLoading, accommodation }
+}
+
+/**
+ * Accommodation component. Displays accommodation details.
+ * @component
+ */
+export default function Accommodation() {
+    useScrollToTop()
+    const navigate = useNavigate()
+    const { isLoading, accommodation } = useAccommodation(logementsData)
+
+    useEffect(() => {
+        if (!accommodation && !isLoading) {
+            navigate('/404') // Redirect to the NotFound page
+        }
+    }, [accommodation, isLoading, navigate])
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    if (!accommodation) {
+        return null
+    }
+
+    const {
+        cover,
+        title,
+        location,
+        host,
+        rating,
+        tags,
+        description,
+        equipments,
+        pictures,
+    } = accommodation
+
+    return (
+        <main className="accommodation container">
+            {pictures ? (
+                <Carousel key={pictures} pictures={pictures} />
+            ) : (
+                <img src={cover} alt="Cover image" />
+            )}
+            <section className="accommodation__summary">
+                <hgroup className="accommodation__heading">
+                    <h1 className="accommodation__title">{title}</h1>
+                    {location && (
+                        <p className="accommodation__location">{location}</p>
+                    )}
+                    <TagList className={`accommodation__tags`} tags={tags} />
+                </hgroup>
+                {host && (
+                    <aside className="accommodation__infos">
+                        <Rating score={rating} activeColor="#ff6060" />
+                        <Person name={host.name} avatar={host.picture} />
+                    </aside>
+                )}
+            </section>
+            <section className="accommodation__details">
+                <h2 className="sr-only">Details</h2>
+                <Accordion title="Description" content={description} />
+                <Accordion title="Équipements" content={equipments} />
+            </section>
+        </main>
+    )
+}
