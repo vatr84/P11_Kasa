22fc37a6699bd06db9664d376d0e0e41964a4cfa
vatr84 <150425169+vatr84@users.noreply.github.com>
2025-01-20 14:53:22 +0100

    Update NotFound.jsx

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -2,7 +2,7 @@ import notFound404 from '../../assets/404.svg'
 import { Link } from 'react-router'
 import './NotFound.scss'
 
-
+// Rend le composant de page NotFound.
 export default function NotFound() {
     return (
         <main className="notFound container">
