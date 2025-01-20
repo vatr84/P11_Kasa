07f60bd193e809964696687950210e8c6bdafffc
vatr84 <150425169+vatr84@users.noreply.github.com>
2025-01-20 14:28:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import { Router as Routes } from './routes/router'
 
-
+// Initialiser et rendre l'application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Routes />
