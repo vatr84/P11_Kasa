d17053432601becaedc73d75a5a03f4f35228b8e
vatr84 <150425169+vatr84@users.noreply.github.com>
2025-01-20 14:59:53 +0100

    Update useScrollToTop.js

--- src/hooks/useScrollToTop.js
+++ src/hooks/useScrollToTop.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 
-
+// La fenêtre défile vers le haut lorsque l'utilisateur accède à une nouvelle page.
 export function useScrollToTop() {
     useEffect(() => {
         window.scrollTo(0, 0)
