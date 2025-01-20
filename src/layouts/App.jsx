03501ed430739bf8eecf90aa5196daf9593b6aa4
vatr84 <150425169+vatr84@users.noreply.github.com>
2025-01-20 14:55:49 +0100

    Update App.jsx

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -2,7 +2,7 @@ import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import { Outlet } from 'react-router'
 
-
+// Le composant de mise en page
 export default function App() {
     return (
         <>
